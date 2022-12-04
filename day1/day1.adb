3a902a7c8b0910777b84d56f8084aa4deab80a8c
Tobias Ulmer <tobiasu@tmux.org>
2022-12-04 21:05:00 +0100

    day1: remove overkill string_to_integer code
    Signed-off-by: Tobias Ulmer <tobiasu@tmux.org>

--- day1/day1.adb
+++ day1/day1.adb
@@ -1,66 +1,8 @@
 with Ada.Text_IO;
 with Ada.Integer_Text_IO;
-with Ada.Assertions;
-with Ada.Strings.Fixed;
 with Ada.Containers.Vectors;
 
 procedure Day1 is
-
-
-   function String_To_Integer(S: String; Base: Integer := 10) return Integer is
-      use Ada.Assertions;
-      use Ada.Strings;
-      Res: Natural := 0;
-      Mult: Natural := 1;
-      N: Natural;
-      Start: Positive;
-      Signedness: Integer;
-   begin
-      if S'Length = 0 then
-         raise Assertion_Error
-            with "can't convert string of length zero";
-      end if;
-
-      case S(S'First) is
-         when '-' =>
-            Start := S'First + 1;
-            Signedness := -1;
-         when '+' =>
-            Start := S'First + 1;
-            Signedness := 1;
-         when others =>
-            Start := S'First;
-            Signedness := 1;
-      end case;
-
-      if S'Last < Start then
-         raise Assertion_Error
-            with "can't convert string of length zero";
-      end if;
-
-      for C: Character of reverse S(Start..S'Last) loop
-         if C in '0'..'9' then
-               N := Character'Pos(C) - Character'Pos('0');
-         elsif C in 'a'..'f' then
-               N := Character'Pos(C) - Character'Pos('a') + 10;
-         elsif C in 'A'..'F' then
-               N := Character'Pos(C) - Character'Pos('A') + 10;
-         else
-               raise Assertion_Error
-                  with "digit '" & C & "' not recognized";
-         end if;
-
-         if N >= Base then
-               raise Assertion_Error
-                  with "digit '" & C & "' too large for base " & Fixed.Trim(Base'Img, Both);
-         end if;
-
-         Res := Res + N * Mult;
-         Mult := Mult * Base;
-      end loop;
-      return Res * Signedness;
-   end;
-
 
    use Ada.Text_IO;
    use Ada.Integer_Text_IO;
@@ -89,7 +31,7 @@ begin
       Get_Line(Input, Buffer, Buffer_Len);
 
       if Buffer_Len > 0 then
-         Calorie := Calorie + String_To_Integer(Buffer(Buffer'First..Buffer_Len));
+         Calorie := Calorie + Natural'Value(Buffer(Buffer'First..Buffer_Len));
       end if;
 
       if (Buffer_Len = 0 or End_Of_File(Input)) and Calorie > 0 then
