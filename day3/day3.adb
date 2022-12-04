ce1baccbe344d235445d3f51ca18f9e0d8ab0511
Tobias Ulmer <tobiasu@tmux.org>
2022-12-04 02:37:26 +0100

    day 3 prep
    Signed-off-by: Tobias Ulmer <tobiasu@tmux.org>

--- day3/day3.adb
+++ day3/day3.adb
@@ -5,22 +5,26 @@ procedure Day3 is
    use Ada.Text_Io;
    use Ada.Integer_Text_Io;
 
-   type Char_Map is array(Character) of Boolean;
+   type Map_Arr is array(Positive range 1..52) of Integer;
 
    procedure Find_Collision(A, B: String; Sum: in out Natural) is
-      Mark: Char_Map := (others => False);
+      Mark: Map_Arr := (others => 0);
+      function To_Idx(C: Character) return Positive is
+      begin
+         if C in 'a'..'z' then
+            return Character'Pos(C)-96;
+         end if;
+         return Character'Pos(C)-38;
+      end To_Idx;
    begin
+
       for C of A loop
-         Mark(C) := True;
+         Mark(To_Idx(C)) := 1;
       end loop;
 
       for C of B loop
-         if Mark(C) then
-            if C in 'a'..'z' then
-               Sum := Sum + Character'Pos(C)-96;
-            else
-               Sum := Sum + Character'Pos(C)-38;
-            end if;
+         if Mark(To_Idx(C)) > 0 then
+               Sum := Sum + To_Idx(C);
             return;
          end if;
       end loop;
