9adb43b6b5f7e8ebe6c9de19572f66927ac1896e
Tobias Ulmer <tobiasu@tmux.org>
2022-12-04 01:45:48 +0100

    day2 part two
    Signed-off-by: Tobias Ulmer <tobiasu@tmux.org>

--- day2/day2.adb
+++ day2/day2.adb
@@ -6,23 +6,37 @@ with Ada.Containers.Vectors;
 
 procedure Day2 is
 
-   type Play_Type is (Rock, Paper, Scissor);
+   type Play_Type is (Rock, Paper, Scissors);
+   type Result_Type is (Lose, Draw, Win);
 
    Total_Score: Natural := 0;
+   Total_Score_Two: Natural := 0;
 
    function Char_To_Play(C: Character) return Play_Type is
    begin
       case C is
          when 'A'|'X' => return Rock;
          when 'B'|'Y' => return Paper;
-         when 'C'|'Z' => return Scissor;
+         when 'C'|'Z' => return Scissors;
          when others =>
             raise Ada.Assertions.Assertion_Error with
                "invalid character";
       end case;
    end Char_To_Play;
 
-   procedure One_Round(Opponent, Player: Play_Type) is
+   function Char_To_Result(C: Character) return Result_Type is
+   begin
+      case C is
+         when 'X' => return Lose;
+         when 'Y' => return Draw;
+         when 'Z' => return Win;
+         when others =>
+            raise Ada.Assertions.Assertion_Error with
+               "invalid character";
+      end case;
+   end Char_To_Result;
+
+   procedure One_Round(Opponent, Player: Play_Type; Total: in out Natural) is
       Score: Natural := Play_Type'Pos(Player) + 1;
       use Ada.Text_IO;
       use Ada.Integer_Text_IO;
@@ -32,27 +46,48 @@ procedure Day2 is
             case Player is
                when Rock => Score := Score + 3;
                when Paper => Score := Score + 6;
-               when Scissor => null;
+               when Scissors => null;
             end case;
          when Paper =>
             case Player is
                when Rock => null;
                when Paper => Score := Score + 3;
-               when Scissor => Score := Score + 6;
+               when Scissors => Score := Score + 6;
             end case;
-         when Scissor =>
+         when Scissors =>
             case Player is
                when Rock => Score := Score + 6;
                when Paper => null;
-               when Scissor => Score := Score + 3;
+               when Scissors => Score := Score + 3;
             end case;
       end case;
-      --Put(Score);
-      --New_Line;
 
-      Total_Score := Total_Score + Score;
+      Total := Total + Score;
 
    end One_Round;
+
+
+   procedure One_Round_Two(Opponent: Play_Type; Result: Result_Type; Total: in out Natural) is
+      Player: Play_Type;
+   begin
+      case Result is
+         when Lose =>
+            case Opponent is
+               when Rock => Player := Scissors;
+               when Paper => Player := Rock;
+               when Scissors => Player := Paper;
+            end case;
+         when Draw =>
+            Player := Opponent;
+         when Win =>
+            case Opponent is
+               when Rock => Player := Paper;
+               when Paper => Player := Scissors;
+               when Scissors => Player := Rock;
+            end case;
+      end case;
+      One_Round(Opponent, Player, Total);
+   end One_Round_Two;
 
 
    use Ada.Text_IO;
@@ -68,12 +103,20 @@ begin
          if Line'Length = 3 then
             One_Round(
                Char_To_Play(Line(Line'First)),
-               Char_To_Play(Line(Line'Last)));
+               Char_To_Play(Line(Line'Last)),
+               Total_Score);
+            One_Round_Two(
+               Char_To_Play(Line(Line'First)),
+               Char_To_Result(Line(Line'Last)),
+               Total_Score_Two);
          end if;
       end;
    end loop;
    Close(Input);
    Put("Total score:");
    Put(Total_Score);
+   New_Line;
+   Put("Total score part two:");
+   Put(Total_Score_Two);
    New_Line;
 end Day2;
