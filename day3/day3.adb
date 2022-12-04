16c7866f030be930125c496b01a4cfc845ae461a
Tobias Ulmer <tobiasu@tmux.org>
2022-12-04 03:12:12 +0100

    day 3 part two
    Signed-off-by: Tobias Ulmer <tobiasu@tmux.org>

--- day3/day3.adb
+++ day3/day3.adb
@@ -1,5 +1,6 @@
-with Ada.Text_Io;
+with Ada.Assertions;
 with Ada.Integer_Text_Io;
+with Ada.Text_Io;
 
 procedure Day3 is
    use Ada.Text_Io;
@@ -7,17 +8,17 @@ procedure Day3 is
 
    type Map_Arr is array(Positive range 1..52) of Integer;
 
+   function To_Idx(C: Character) return Positive is
+   begin
+      if C in 'a'..'z' then
+         return Character'Pos(C)-96;
+      end if;
+      return Character'Pos(C)-38;
+   end To_Idx;
+
    procedure Find_Collision(A, B: String; Sum: in out Natural) is
       Mark: Map_Arr := (others => 0);
-      function To_Idx(C: Character) return Positive is
-      begin
-         if C in 'a'..'z' then
-            return Character'Pos(C)-96;
-         end if;
-         return Character'Pos(C)-38;
-      end To_Idx;
    begin
-
       for C of A loop
          Mark(To_Idx(C)) := 1;
       end loop;
@@ -30,8 +31,37 @@ procedure Day3 is
       end loop;
    end Find_Collision;
 
+   procedure Find_Collision_Three(A, B, C: String; Sum: in out Natural) is
+      Mark: Map_Arr := (others => 0);
+      Merge: Map_Arr := (others => 0);
+   begin
+      for I of A loop
+         Mark(To_Idx(I)) := 1;
+      end loop;
+      Merge := Mark;
+
+      Mark := (others => 0);
+      for I of B loop
+         Mark(To_Idx(I)) := 1;
+      end loop;
+
+      for J in Merge'Range loop
+         Merge(J) := Merge(J) + Mark(J);
+      end loop;
+
+      for I of C loop
+         if Merge(To_Idx(I)) = 2 then
+            Sum := Sum + To_Idx(I);
+            return;
+         end if;
+      end loop;
+   end Find_Collision_Three;
+
    Input: File_Type;
    Sum: Natural := 0;
+   Group_Cnt: Natural := 0;
+   A, B: String(1..256);
+   A_Last, B_Last: Positive;
 begin
    Open(Input, In_File, "input.txt");
    while not End_Of_File(Input) loop
@@ -44,8 +74,44 @@ begin
             Sum);
       end;
    end loop;
+   Close(Input);
 
    Put("Sum:");
+   Put(Sum);
+   New_Line;
+
+   Sum := 0;
+   Open(Input, In_File, "input.txt");
+   while not End_Of_File(Input) loop
+      declare
+         Line: String := Get_Line(Input);
+      begin
+         case Group_Cnt is
+            when 0 =>
+               A(Line'Range) := Line;
+               A_Last := Line'Last;
+            when 1 =>
+               B(Line'Range) := Line;
+               B_Last := Line'Last;
+            when 2 =>
+               null;
+            when others =>
+               raise Ada.Assertions.Assertion_Error;
+         end case;
+
+         Group_Cnt := Group_Cnt + 1;
+         if Group_Cnt rem 3 = 0 then
+            Group_Cnt := 0;
+            Find_Collision_Three(
+               A(A'First..A_Last),
+               B(B'First..B_Last),
+               Line, Sum);
+         end if;
+      end;
+   end loop;
+   Close(Input);
+
+   Put("Threeway Sum:");
    Put(Sum);
    New_Line;
 end;
